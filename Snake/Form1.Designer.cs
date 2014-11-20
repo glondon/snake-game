b1f08ca432f9b737c21ce079fa850ec4886d2060
glondon <gregdlondon@gmail.com>
2014-11-20 16:23:34 -0600

    Added files to project folder

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbCanvis = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvis)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbCanvis
+            // 
+            this.pbCanvis.Location = new System.Drawing.Point(12, 12);
+            this.pbCanvis.Name = "pbCanvis";
+            this.pbCanvis.Size = new System.Drawing.Size(467, 306);
+            this.pbCanvis.TabIndex = 0;
+            this.pbCanvis.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(502, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Score:";
+            // 
+            // lbScore
+            // 
+            this.lbScore.AutoSize = true;
+            this.lbScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbScore.Location = new System.Drawing.Point(606, 12);
+            this.lbScore.Name = "lbScore";
+            this.lbScore.Size = new System.Drawing.Size(0, 37);
+            this.lbScore.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 330);
+            this.Controls.Add(this.lbScore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbCanvis);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvis)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbCanvis;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Snake/Input.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Input
+    {
+        
+        //Load list of available keyboard buttons
+        private static Hashtable keyTable = new Hashtable();
+
+        //Perform a check to see if a particular button is pressed
+        public static bool KeyPressed(Keys key)
+        {
+            if (keyTable[key] == null)
+            {
+                return false;
+            }
+
+            return (bool)keyTable[key]; 
+        }
+
+        //Detect if a keyboard button is pressed
+        public static void ChangeState(Keys key, bool state)
+        {
+            keyTable[key] = state;
+        }
+         
+    }
+}
